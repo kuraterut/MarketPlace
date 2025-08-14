a033c4a73b261f2f9c301edcd3a8ba33a9432c0c
kuraterut <kurylilia@gmail.com>
2025-08-14 13:48:54 +0300

    fix product integration test

--- ProductService/src/test/java/integration/ProductServiceIntegrationTest.java
+++ ProductService/src/test/java/integration/ProductServiceIntegrationTest.java
@@ -3,12 +3,18 @@ package integration;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.kuraterut.productservice.ProductServiceApplication;
+import org.kuraterut.productservice.dto.requests.CreateCategoryRequest;
+import org.kuraterut.productservice.dto.requests.CreateProductRequest;
+import org.kuraterut.productservice.service.CategoryService;
 import org.kuraterut.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.redis.cache.RedisCache;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
@@ -20,7 +26,10 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.math.BigDecimal;
 import java.time.Duration;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -69,21 +78,56 @@ public class ProductServiceIntegrationTest {
     private ProductService productService;
 
     @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
     private CacheManager cacheManager;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void printRedisCacheContents() {
+        // Получаем все ключи по шаблону (для кэша 'products')
+        Set<String> keys = redisTemplate.keys("products::*");
+
+        if (keys != null) {
+            keys.forEach(key -> {
+                Object value = redisTemplate.opsForValue().get(key);
+                System.out.println("Key: " + key + " | Value: " + value);
+            });
+        }
+    }
+
     @Test
-    @Disabled("Temporarily disabled due to Redis issues in Jenkins environment")
+//    @Disabled("Temporarily disabled due to Redis issues in Jenkins environment")
     void cacheWorksForGetAllProducts() {
+        CreateCategoryRequest createCategoryRequest = new CreateCategoryRequest(
+                "testCategoryName",
+                "testCategoryDescription"
+        );
+        categoryService.createCategory(createCategoryRequest);
+
+        CreateProductRequest createProductRequest = new CreateProductRequest(
+                "testProductName",
+                "testProductDescription",
+                BigDecimal.TEN,
+                "testCategoryName",
+                5L
+        );
+        productService.createProduct(createProductRequest, 1L);
+
         var pageable = PageRequest.of(0, 5);
 
         // First call - should populate cache
         var firstCall = productService.getAllProducts(pageable);
+
 
         // Verify cache entry exists
         await()
                 .atMost(Duration.ofSeconds(30))
                 .pollInterval(Duration.ofSeconds(1))
                 .untilAsserted(() -> {
+                    printRedisCacheContents();
                     var cache = cacheManager.getCache("products");
                     assertThat(cache).isNotNull();
                     var cachedValue = cache.get("all_products_0_5");
